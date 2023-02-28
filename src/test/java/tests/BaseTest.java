89c3aed322085758ba5ef90185b49b6c0901183c
Vitaly Petkun <vitaly.petkun@gmail.com>
2023-02-28 17:43:39 +0300

    Add file BaseTest.java

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import framework.browser.Browser;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    @BeforeMethod
+    protected void setUp() {
+        Browser.setMaximizeWindow();
+        Browser.timeouts();
+    }
+
+    @AfterMethod
+    protected void quitDriver() {
+        Browser.quit();
+    }
+}
