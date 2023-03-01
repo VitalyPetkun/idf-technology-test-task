fa69f3377cfa840851ff7b1199077b63b22677ea
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2023-03-01 09:09:11 +0300

    Add file BookStoreApplicationPage.java

---
+++ src/main/java/pages/BookStoreApplicationPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import framework.BaseForm;
+import framework.elements.Button;
+import framework.elements.Link;
+import org.openqa.selenium.By;
+
+public class BookStoreApplicationPage extends BaseForm {
+
+    private static final Button BOOK_STORE_APPLICATION_PAGE_UNIQ_ELEMENT = new Button(
+            By.xpath("//div[contains(@class,'books')]/div[@id='searchBox-wrapper']"), "Book Store Application page uniq element"
+    );
+    private static final Button LOGIN_BTN = new Button(By.xpath("//button[@id='login']"), "Login button");
+    private static final Link BOOKS_TITLES_LNK = new Link(By.xpath("//div[contains(@class,'td')]//span/a"), "Books titles link");
+
+    private BookStoreApplicationPage() {
+        super(BOOK_STORE_APPLICATION_PAGE_UNIQ_ELEMENT, "Book Store Application page");
+    }
+
+    public static void clickLoginBtn() {
+        LOGIN_BTN.click();
+    }
+
+    public static String getTextBooksTitlesLnk() {
+        return BOOKS_TITLES_LNK.getText();
+    }
+}
